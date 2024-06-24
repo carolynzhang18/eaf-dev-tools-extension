bc7d22b58ff72fdc068e7b61759ddcaee3bba92f
Carolyn Zhang <carolyn.zhang18@gmail.com>
2024-06-23 22:24:04 -0400

    Restyle App

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
-import React from 'react';
 import styled from '@emotion/styled';
+import { Divider } from '@mui/material';
 import Utilities from './Utilities';
 import Links from './Links';
+import { useTheme } from '@mui/material';
 
 const StyledDiv = styled.div`
-  width: 240px;
-  padding: 0px 5px;
+  width: 270px;
+  background-color: #fafafa;
+  margin: -10px;
+  padding: 5px 15px;
 `;
 
 const Title = styled.h1`
   font-size: 18px;
 `;
 
-const Subtitle = styled.h2`
+type SubtitleProps = {
+  $color: string;
+};
+
+const Subtitle = styled.h2<SubtitleProps>`
+  color: ${props => props.$color};
   font-size: 14px;
+  margin-bottom: -10px;
 `;
 
 const App = () => {
+  const theme = useTheme();
   return (
     <StyledDiv>
-      <Title>EAF Dev Tools 😎</Title>
-      <Subtitle>Utilities</Subtitle>
-      <Utilities />
-      <Subtitle>Links</Subtitle>
-      <Links />
+      <main>
+        <Title>EAF Dev Tools 😎</Title>
+        <Divider />
+        <Subtitle $color={theme.palette.primary.dark}>Utilities</Subtitle>
+        <Utilities />
+        <br />
+        <Divider />
+        <Subtitle $color={theme.palette.primary.dark}>Links</Subtitle>
+        <Links />
+        <br />
+      </main>
     </StyledDiv>
   );
 }
---
+++ src/labels.ts
@@ -0,0 +1,9 @@
+const labels = {
+  copyAccessToken: "Copy Access Token",
+  verifyAccessibility: "Verify Accessibility",
+  github: "GitHub Repo",
+  taskBoard: "Task Board",
+  accessibilityChecklist: "Accessibility Checklist"
+};
+
+export default labels;
